cdc75880d58f6cb24ddd672218f513b9e2ac6b11
Aniket Chhari <aniketsingh20122012@gmail.com>
2024-05-07 00:58:38 +0530

    Auth Added

--- src/App.jsx
+++ src/App.jsx
@@ -12,31 +12,34 @@ import City from "./Components/City/City";
 import Form from "./Components/Form/Form";
 
 import { CitiesProvider } from "./Context/CitiesContext";
+import { AuthProvider } from "./Context/AuthProvider";
 
 function App() {
   return (
-    <CitiesProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<HomePage />} />
-          <Route path="pricing" element={<Pricing />} />
-          <Route path="product" element={<Product />} />
-          <Route path="login" element={<Login />} />
+    <AuthProvider>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<HomePage />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="product" element={<Product />} />
+            <Route path="login" element={<Login />} />
 
-          <Route path="app" element={<AppLayout />}>
-            <Route index element={<Navigate replace to="cities" />} />
+            <Route path="app" element={<AppLayout />}>
+              <Route index element={<Navigate replace to="cities" />} />
 
-            <Route path="cities" element={<CityList />} />
+              <Route path="cities" element={<CityList />} />
 
-            <Route path="cities/:id" element={<City />} />
+              <Route path="cities/:id" element={<City />} />
 
-            <Route path="countries" element={<CountryList />} />
-            <Route path="form" element={<Form />} />
-          </Route>
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </CitiesProvider>
+              <Route path="countries" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
+            </Route>
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider>
   );
 }
 
--- src/Pages/AppLayout.jsx
+++ src/Pages/AppLayout.jsx
@@ -1,12 +1,14 @@
 import Sidebar from "../Components/Sidebar/Sidebar";
 import Map from "../Components/Map/Map";
 import styles from "./AppLayout.module.css";
+import User from "../Components/User/User";
 
 function AppLayout() {
   return (
     <div className={styles.app}>
       <Sidebar />
       <Map />
+      <User />
     </div>
   );
 }
